526678896567bc23563dc4de9c9b58e42dfc079e
Waqas Tahir <wakaztahir@gmail.com>
2023-05-11 15:28:34 +0500

    pull refresh ready

---
+++ pullrefresh/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    kotlin("multiplatform")
+    id("maven-publish")
+    id("org.jetbrains.compose")
+    id("com.android.library")
+}
+
+group = "com.qawaz"
+version = findProperty("version") as String
+
+kotlin {
+    android {
+        publishLibraryVariants("release")
+    }
+    jvm("desktop") {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+    }
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(compose.runtime)
+                api(compose.foundation)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                api(compose.material3)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+
+            }
+        }
+        val desktopMain by getting {
+            dependencies {
+                api(compose.preview)
+            }
+        }
+        val desktopTest by getting
+
+        val jsMain by getting {
+            dependencies {
+
+            }
+        }
+    }
+}
+
+android {
+    compileSdk = 33
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 21
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+publishing {
+    repositories {
+        maven("https://maven.pkg.github.com/Qawaz/material3-pullrefresh") {
+            name = "GithubPackages"
+            credentials {
+                username = (System.getenv("GPR_USER")).toString()
+                password = (System.getenv("GPR_API_KEY")).toString()
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,45 +1,19 @@
-@file:Suppress("UnstableApiUsage")
-
-rootProject.name = "androidx-compose-material3-pullrefresh"
-
 pluginManagement {
     repositories {
         google()
+        gradlePluginPortal()
         mavenCentral()
-        gradlePluginPortal {
-            content {
-                includeGroupByRegex("com.gradle.*")
-            }
-        }
-        plugins {
-            id("com.android.library") version extra["project.android.gradle.plugin.version"] as String
-            id("com.gradle.enterprise") version extra["project.gradle.enterprise.plugin.version"] as String
-            kotlin("android") version extra["project.kotlin.version"] as String
-        }
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        kotlin("android").version(extra["kotlin.version"] as String)
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        id("com.android.application").version(extra["agp.version"] as String)
+        id("com.android.library").version(extra["agp.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
     }
 }
+rootProject.name = "Material3PullRefresh"
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-plugins {
-    id("com.gradle.enterprise") apply false
-}
-
-if (gradle.parent == null) {
-    apply(plugin = "com.gradle.enterprise")
-    gradleEnterprise {
-        buildScan {
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-            publishAlwaysIf(!gradle.startParameter.isOffline)
-        }
-    }
-}
-
-include("library")
+include(":pullrefresh")
